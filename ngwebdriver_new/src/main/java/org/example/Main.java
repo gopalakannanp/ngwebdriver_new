4e8d83e2041053c3285cbd844be1918e1d387c25
gopalakannanp <36475187+gopalakannanp@users.noreply.github.com>
2022-12-08 19:10:58 +0530

    Add files via upload

---
+++ ngwebdriver_new/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    WebDriver driver;
+
+    JavascriptExecutor js;
+
+
+
+
+
+
+
+}
